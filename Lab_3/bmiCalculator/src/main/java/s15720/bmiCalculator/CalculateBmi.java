06eb22291c8421b8aa69eb2c4e0b13c7a783aa8b
Daiv <dawid.berk@gmail.com>
2020-12-06 12:17:47 +0100

    add old files to new Maven project

---
+++ Lab_3/bmiCalculator/src/main/java/s15720/bmiCalculator/CalculateBmi.java
@@ -0,0 +1,67 @@
+package s15720.bmiCalculator;
+
+public class CalculateBmi {
+	
+	public float CalculateBMI(float height, float weight, float age) {
+		
+        String heightStr = String.valueOf(height);
+        String weightStr = String.valueOf(weight);
+        String ageStr = String.valueOf(age);
+        float result = 0;
+        
+        if (heightStr == null || "".equals(heightStr)
+                || weightStr == null || "".equals(weightStr)) 
+        	throw new IllegalArgumentException("Wrong data");
+        else{
+
+            float heightValue = Float.parseFloat(heightStr) / 100;
+            float weightValue = Float.parseFloat(weightStr);
+
+            result = weightValue / (heightValue * heightValue);
+        }
+        
+        return result;
+    }
+
+	public float CalculateBHC(String heightStr, String weightStr, String ageStr, String sex) {
+        
+    	float heightValue = Float.parseFloat(heightStr);
+        float weightValue = Float.parseFloat(weightStr);
+        float ageValue = Float.parseFloat(ageStr);
+        float result = 0;
+
+
+        if(sex.equals(("female"))) {
+            result = 655.1f + (9.563f * weightValue) + (1.85f * heightValue) - (4.676f * ageValue);
+        }else if(sex.equals(("male"))) {
+            result = 66.5f + (13.75f * weightValue) + (5.003f * heightValue) - (6.775f * ageValue);
+        }
+
+        return result;
+    }
+	
+	public String DisplayBMI(float bmi) {
+
+		String bmiInfo;
+		
+        if (Float.compare(bmi, 15f) <= 0) {
+        	bmiInfo = "wyglodzenie";
+        } else if (Float.compare(bmi, 15f) > 0 && Float.compare(bmi, 16f) <= 0) {
+        	bmiInfo = "wychudzenie";
+        } else if (Float.compare(bmi, 16f) > 0 && Float.compare(bmi, 18.5f) <= 0) {
+        	bmiInfo = "niedowaga";
+        } else if (Float.compare(bmi, 18.5f) > 0 && Float.compare(bmi, 25f) <= 0) {
+        	bmiInfo = "wartosc_prawidlowa";
+        } else if (Float.compare(bmi, 25f) > 0 && Float.compare(bmi, 30f) <= 0) {
+        	bmiInfo = "nadwaga";
+        } else if (Float.compare(bmi, 30f) > 0 && Float.compare(bmi, 35f) <= 0) {
+        	bmiInfo = "I_stopien_otylosci";
+        } else if (Float.compare(bmi, 35f) > 0 && Float.compare(bmi, 40f) <= 0) {
+        	bmiInfo = "II_stopien_otylosci";
+        } else {
+        	bmiInfo = "otyłosc_skrajna";
+        }
+
+        return bmiInfo;
+    }
+}
--- Lab_3/bmiCalculator/src/test/java/s15720/bmiCalculator/AppTest.java
+++ Lab_3/bmiCalculator/src/test/java/s15720/bmiCalculator/AppTest.java
@@ -1,20 +1,44 @@
 package s15720.bmiCalculator;
 
-import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+	private CalculateBmi calculateBMI;
+	private CalculateBmi displayBMI;
+
+	@Before
+	public void setUp() {
+		calculateBMI = new CalculateBmi();
+		displayBMI = new CalculateBmi();
+	}
+	
+	@After
+	public void tearDown() {
+		calculateBMI = null;
+		displayBMI = null;
+	}
+	
+	@Test
+    public void bmi_isCorrect() {
+        float result = calculateBMI.CalculateBMI(182.0f, 80.0f, 20.0f);
+		assertEquals(24.151672f, result, 0);
+		System.out.println("Test bmi_isCorrect result: " + result);
     }
+	
+	@Test
+	public void DataIsNotNull() {
+		float result = calculateBMI.CalculateBMI(182.0f, 80.0f, 20.0f);
+		assertNotNull("Result ", result);
+	}
+	
+	@Test
+	public void BmiInfo_isCorrect() {
+		String result = displayBMI.DisplayBMI(26.0f);
+		assertEquals("nadwaga", result);
+	}
 }
